f0425284270fdb4051a308fe775bdd4d3c19d8e3
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2024-01-23 16:13:25 +0530

    LeetCode problem no. 55

---
+++ Day_4.2_Jump_Game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int canReach = 0;
+        for (int i = 0; i < nums.length; i++){
+            if(i>canReach)return false;
+                canReach = Math.max(canReach, nums[i] + i);
+        }
+            return true;
+    }
+}
