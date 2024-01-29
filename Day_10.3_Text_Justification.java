5731da78f0af506c13a24a105fae1e65d3967108
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2024-01-29 16:40:30 +0530

    LeetCode problem no. 68

---
+++ Day_10.3_Text_Justification.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<String>();
+        int word=0;
+        while(word<words.length)
+        {
+            int j=word-1;
+            int characters=0;
+            while(j+1<words.length && characters+words[j+1].length() + j+1-word<=maxWidth)
+            {
+                j++;
+                characters+=words[j].length();
+            }
+            result.add(line(words,word,j,characters,maxWidth));
+            word=j+1;
+        }
+        return result;
+    }
+    public String line(String words[],int start,int end, int Linelen,int max)
+    {
+        StringBuilder a = new StringBuilder();
+        int p=1,q=0;
+        if(end!=start)
+        {
+            p=(max-Linelen)/(end-start);
+            q=(max-Linelen)%(end-start);
+        }
+        
+        for(int i=start;i<=end;i++)
+        {
+            a.append(words[i]);
+            if(i!=end)
+            {
+                if(end==words.length-1) a.append(" ");
+                else {
+                    for(int j=1;j<=p;j++) a.append(" ");
+                    if(q-->=1) a.append(" ");
+                }
+            }
+        }
+        while(a.length()<max) a.append(" ");
+        return a.toString();
+    }
+}
