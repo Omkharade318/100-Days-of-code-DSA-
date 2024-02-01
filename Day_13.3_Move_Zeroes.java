d8e540fe08ce3d317a6f662734d83fdd45dddbfd
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2024-02-01 21:44:55 +0530

    LeetCode problem no. 283

---
+++ Day_13.3_Move_Zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0 ){
+               count++;
+            }
+
+            else if(count > 0){
+                int temp = nums[i];
+                nums[i]  = 0;
+                nums[i - count] = temp;
+            }
+        }
+    }
+}
