401a7383cdcc783c9f4ff5a145d3857c7cad5883
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2024-02-11 12:21:07 +0530

    LeetCode problem no. 219

---
+++ Day_23.2_Contains_Duplicate_II.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> window = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                window.remove(nums[i - k - 1]);
+            }
+            if (window.contains(nums[i])) {
+                return true;
+            }
+            window.add(nums[i]);
+        }
+        return false;
+    }
+}
