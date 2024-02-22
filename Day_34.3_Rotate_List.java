dc8b6b48f186d6c8408aafe9668e7b75c98f7dfd
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2024-02-22 17:22:58 +0530

    LeetCode problem no. 61

---
+++ Day_34.3_Rotate_List.java
@@ -0,0 +1,32 @@
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) 
+    {
+        if( k <= 0 || head == null || head.next == null)
+        return head;
+
+        ListNode last = head;
+        int length = 1;
+        while(last.next != null)
+        {
+            last = last.next;
+            length++;
+
+        }
+        last.next = head;
+
+        int rotation = k % length;
+        int skip = length - rotation;
+
+        ListNode newLast = head;
+        for(int i = 0; i < skip -1; i++)
+        {
+            newLast = newLast.next;
+
+        }
+        head = newLast.next;
+        newLast.next = null;
+
+        return head;
+        
+    }
+}
