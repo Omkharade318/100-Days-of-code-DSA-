55da860153cbe5b46cc20ac5d03028eb53fdb360
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2024-02-29 18:52:11 +0530

    LeetCode problem no. 112

--- Day_41.2_Path_Sum.java
+++ Day_41.2_Path_Sum.java
@@ -1,4 +1,4 @@
-class Solution {
+ class Solution {
         public static boolean hasPathSum(TreeNode root, int k) {
         if (root == null) return false;
         if (root.left == null && root.right == null) return root.val == k;
