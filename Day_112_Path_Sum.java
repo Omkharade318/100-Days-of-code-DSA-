c47522010f5d10babe37aa1fa568dd0d7b3962ad
Om kharade <146190558+Omkharade318@users.noreply.github.com>
2024-02-29 18:49:46 +0530

    Create Day_112_Path_Sum.java

---
+++ Day_112_Path_Sum.java
@@ -0,0 +1,7 @@
+class Solution {
+        public static boolean hasPathSum(TreeNode root, int k) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return root.val == k;
+        return hasPathSum(root.left, k - root.val) || hasPathSum(root.right, k - root.val);
+    }
+}
